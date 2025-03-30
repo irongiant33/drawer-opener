fb479abe57adeb6e648d448a6530ad11e8290a29
irongiant33 <samuelmiller0714@gmail.com>
2025-03-30 12:04:11 -0400

    MVP of the motor distance calibration complete. Succcessfully moves until both limit switches hit and then moves fast between the switches.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Drawer Opener
 
+
 ## Resources
 
 - https://lastminuteengineers.com/drv8825-stepper-motor-driver-arduino-tutorial/
--- motor-distance-calibration/motor-distance-calibration.ino
+++ motor-distance-calibration/motor-distance-calibration.ino
@@ -11,6 +11,11 @@ const unsigned long DEBOUNCE_DELAY = 50; //debounce time in milliseconds; increa
 const int INITIAL_LED_STATE = LOW;       
 const int INITIAL_SWITCH_STATE = LOW;
 const int MOTOR_INTERFACE_TYPE = AccelStepper::DRIVER;
+const int CALIBRATION_SPEED = 200;
+const int OPERATIONAL_SPEED = 4000;
+const int ACCELERATION = 100;
+const int STOP_ACCELERATION = 300;
+const int STEP_SIZE = 10;
 
 //global structs
 typedef struct limit_switch_struct{
@@ -27,9 +32,8 @@ typedef struct limit_switch_struct{
 limit_switch lower_switch = {I_LOWER_SWITCH, O_LOWER_LED, INITIAL_SWITCH_STATE, INITIAL_LED_STATE, INITIAL_SWITCH_STATE, 0, -1};
 limit_switch upper_switch = {I_UPPER_SWITCH, O_UPPER_LED, INITIAL_SWITCH_STATE, INITIAL_LED_STATE, INITIAL_SWITCH_STATE, 0, -1};
 AccelStepper myStepper(MOTOR_INTERFACE_TYPE, O_STEP, O_DIR);
-bool home_found = false;
-int direction = -10; //find the negative limit first to set home point
-int count = 0;
+bool calibrated = false;
+int direction = -1; //find the negative limit first to set home point
 
 void setup() {
   Serial.begin(9600);
@@ -43,9 +47,9 @@ void setup() {
   pinMode(O_UPPER_LED, OUTPUT);
   digitalWrite(O_UPPER_LED, upper_switch.led_state);
 
-  myStepper.setMaxSpeed(200);
-	myStepper.setAcceleration(100);
-	myStepper.setSpeed(200);
+  myStepper.setMaxSpeed(OPERATIONAL_SPEED);
+	myStepper.setAcceleration(ACCELERATION);
+	myStepper.setSpeed(direction * CALIBRATION_SPEED);
   // TODO: logic to move until you hit a limit switch?
 	//myStepper.moveTo(-20000); //find the negative limit first to set home point
 }
@@ -53,13 +57,13 @@ void setup() {
 long stop_now()
 {
   long current_position = myStepper.currentPosition();
-  myStepper.setAcceleration(300);
+  //myStepper.setAcceleration(direction * - 1 * STOP_ACCELERATION);
   myStepper.stop();
-  myStepper.runToPosition();
+  //myStepper.runToPosition();
   return current_position;
 }
 
-bool read_pin(limit_switch my_switch)
+bool read_pin(limit_switch &my_switch)
 {
   bool is_my_switch_hit = false;
   int reading = digitalRead(my_switch.switch_pin);
@@ -77,15 +81,7 @@ bool read_pin(limit_switch my_switch)
       {
         long current_position = stop_now();
         my_switch.led_state = !my_switch.led_state;
-        if(home_found && my_switch.limit < 0)
-        {
-          my_switch.limit = current_position;
-        }
-        else if(my_switch.limit < 0)
-        {
-          home_found = true;
-          my_switch.limit = 0; //home point found 
-        }
+        my_switch.limit = current_position;
         is_my_switch_hit = true;
       }
     }
@@ -99,33 +95,44 @@ bool read_pin(limit_switch my_switch)
 void loop() {
   // put your main code here, to run repeatedly:
   bool is_lower_hit = read_pin(lower_switch);
-  if(is_lower_hit && upper_switch.limit < 0)
+  if(is_lower_hit)
   {
     direction = direction * -1;
-    //myStepper.moveTo(20000);
-  }
-  else if(is_lower_hit)
-  {
-    myStepper.moveTo(upper_switch.limit);
+    Serial.print("lower switch hit, changing direction to ");
+    Serial.println(direction);
+    calibrated = true; //expected to hit upper first, so hitting this means we're done calibrating
+    //myStepper.setSpeed(direction * OPERATIONAL_SPEED);
   }
   bool is_upper_hit = read_pin(upper_switch);
-  if(is_upper_hit && lower_switch.limit < 0)
+  if(is_upper_hit)
   {
     direction = direction * -1;
-    //myStepper.moveTo(20000);
-  }
-  else if(is_upper_hit)
-  {
-    myStepper.moveTo(lower_switch.limit);
+    Serial.print("upper switch hit, changing direction to ");
+    Serial.println(direction);
   }
 
-  if(!is_lower_hit && !is_upper_hit)
+  if(!calibrated && !is_lower_hit && !is_upper_hit)
   {
-    myStepper.move(direction);
-    myStepper.setSpeed(200);
-    myStepper.run();
+    myStepper.move(direction * STEP_SIZE);
+    myStepper.setSpeed(direction * CALIBRATION_SPEED);
   }
-  count = count + 1;
-  Serial.print("looping");
-  Serial.println(count);
+  else if(calibrated)
+  {
+    if(direction < 0)
+    {
+      myStepper.moveTo(upper_switch.limit);
+    }
+    else
+    {
+      myStepper.moveTo(lower_switch.limit);
+    }
+
+    if (myStepper.distanceToGo() == 0)
+    {
+      direction = direction * -1;
+      //myStepper.setSpeed(direction * OPERATIONAL_SPEED);
+    }
+  }
+
+  myStepper.run();
 }
